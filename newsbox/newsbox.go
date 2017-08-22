8137247df1f924d14e952ba66ba32e89645d2c8d
David Hernandez <dahernan@gmail.com>
2017-08-22 17:30:20 +0100

    newsbox api

---
+++ newsbox/newsbox.go
@@ -0,0 +1,165 @@
+// Package newsbox provides a client for accessing newsbox services.
+package newsbox
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/machinebox/sdk-go/x/boxutil"
+	"github.com/pkg/errors"
+)
+
+// Analysis represents an analysis of title, content and domain.
+type Analysis struct {
+	// Title is the response object for the title analysis
+	Title Title `json:"title"`
+	// Content is the response object for the content analysis
+	Content Content `json:"content"`
+	// Domain is the response object for the domain analysis
+	Domain Domain `json:"domain"`
+}
+
+type Title struct {
+	// Decision is the string representing the decision could be bias/unsure/impartial
+	Decision string `json:"decision,omitempty"`
+	// Score is the numeric score of the decision is between 0.00 (bias) and 1.00 (impartial)
+	Score float64 `json:"score,omitempty"`
+	// Entities represents entities discovered in the text.
+	Entities []Entity `json:"entities,omitempty"`
+}
+
+type Content struct {
+	// Decision is the string representing the decision could be bias/unsure/impartial
+	Decision string `json:"decision,omitempty"`
+	// Score is the numeric score of the decision is between 0.00 (bias) and 1.00 (impartial)
+	Score float64 `json:"score,omitempty"`
+	// Entities represents entities discovered in the text.
+	Entities []Entity `json:"entities,omitempty"`
+	// Keywords are the most relevant keywords extracted from the text
+	Keywords []Keyword `json:"keywords"`
+}
+
+type Domain struct {
+	// Domain is the domain extracted from the URL
+	Domain string `json:"domain,omitempty"`
+	// Category is one of the listed on the API docs
+	Category string `json:"category,omitempty"`
+}
+
+// Entity represents an entity discovered in the text.
+type Entity struct {
+	// Type is a string describing the kind of entity.
+	Type string `json:"type"`
+	// Text is the text of the entity.
+	Text string `json:"text"`
+	// Start is the absolute start position of the entity (in the original text).
+	Start int `json:"start"`
+	// Start is the absolute end position of the entity (in the original text).
+	End int `json:"end"`
+}
+
+// Keyword represents a key word.
+type Keyword struct {
+	Keyword string `json:"keyword"`
+}
+
+// Client is an HTTP client that can make requests to the box.
+type Client struct {
+	addr string
+
+	// HTTPClient is the http.Client that will be used to
+	// make requests.
+	HTTPClient *http.Client
+}
+
+// New makes a new Client.
+func New(addr string) *Client {
+	return &Client{
+		addr: addr,
+		HTTPClient: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}
+}
+
+// Info gets the details about the box.
+func (c *Client) Info() (*boxutil.Info, error) {
+	var info boxutil.Info
+	u, err := url.Parse(c.addr + "/info")
+	if err != nil {
+		return nil, err
+	}
+	if !u.IsAbs() {
+		return nil, errors.New("box address must be absolute")
+	}
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Accept", "application/json; charset=utf-8")
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if err := json.NewDecoder(resp.Body).Decode(&info); err != nil {
+		return nil, err
+	}
+	return &info, nil
+}
+
+// Check passes the text from the Reader to newsbox for analysis.
+func (c *Client) Check(title string, content string, u *url.URL) (*Analysis, error) {
+	uu, err := url.Parse(c.addr + "/newsbox/check")
+	if err != nil {
+		return nil, err
+	}
+	if !u.IsAbs() {
+		return nil, errors.New("box address must be absolute")
+	}
+	vals := url.Values{}
+	vals.Set("title", title)
+	vals.Set("content", content)
+	vals.Set("url", u.String())
+
+	req, err := http.NewRequest("POST", uu.String(), strings.NewReader(vals.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Accept", "application/json; charset=utf-8")
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var response struct {
+		Success bool
+		Error   string
+
+		Title   Title   `json:"title"`
+		Content Content `json:"content"`
+		Domain  Domain  `json:"domain"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return nil, errors.Wrap(err, "decoding response")
+	}
+	if !response.Success {
+		return nil, ErrNewsbox(response.Error)
+	}
+	return &Analysis{
+		Title:   response.Title,
+		Content: response.Content,
+		Domain:  response.Domain,
+	}, nil
+}
+
+// ErrNewsbox represents an error from newsbox.
+type ErrNewsbox string
+
+func (e ErrNewsbox) Error() string {
+	return "newsbox: " + string(e)
+}
---
+++ newsbox/newsbox_test.go
@@ -0,0 +1,166 @@
+package newsbox_test
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/machinebox/sdk-go/newsbox"
+	"github.com/matryer/is"
+)
+
+func TestCheck(t *testing.T) {
+	result := `
+	{
+	"success": true,
+	"title": {
+		"decision": "impartial",
+		"score": 0.7757045030593872,
+		"entities": [
+			{
+				"text": "China",
+				"start": 0,
+				"end": 4,
+				"type": "place"
+			}
+		]
+	},
+	"content": {
+		"decision": "bias",
+		"score": 0.33232277631759644,
+		"entities": [
+			{
+				"text": "Fuxing",
+				"start": 21,
+				"end": 26,
+				"type": "place"
+			},
+			{
+				"text": "300",
+				"start": 74,
+				"end": 76,
+				"type": "cardinal"
+			},
+			{
+				"text": "186mph",
+				"start": 83,
+				"end": 88,
+				"type": "quantity"
+			},
+			{
+				"text": "2011",
+				"start": 94,
+				"end": 97,
+				"type": "date"
+			},
+			{
+				"text": "two",
+				"start": 109,
+				"end": 111,
+				"type": "cardinal"
+			},
+			{
+				"text": "40",
+				"start": 133,
+				"end": 134,
+				"type": "cardinal"
+			},
+			{
+				"text": "next week",
+				"start": 149,
+				"end": 157,
+				"type": "date"
+			},
+			{
+				"text": "about 350",
+				"start": 234,
+				"end": 242,
+				"type": "cardinal"
+			}
+		],
+		"keywords": [
+			{
+				"keyword": "high speed"
+			},
+			{
+				"keyword": "bullet train"
+			},
+			{
+				"keyword": "speed"
+			},
+			{
+				"keyword": "train"
+			},
+			{
+				"keyword": "mph"
+			},
+			{
+				"keyword": "km/h"
+			},
+			{
+				"keyword": "rejuvenation"
+			},
+			{
+				"keyword": "fuxing"
+			},
+			{
+				"keyword": "crash"
+			},
+			{
+				"keyword": "people"
+			}
+		]
+	},
+	"domain": {
+		"domain": "bbc.co.uk",
+		"category": "trusted"
+	}
+	}
+	`
+	is := is.New(t)
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		is.Equal(r.Method, "POST")
+		is.Equal(r.URL.Path, "/newsbox/check")
+		is.Equal(r.Header.Get("Accept"), "application/json; charset=utf-8")
+		io.WriteString(w, result)
+	}))
+	defer srv.Close()
+	u, err := url.Parse("http://www.bbc.co.uk/news/technology-41011662")
+	is.NoErr(err)
+
+	tb := newsbox.New(srv.URL)
+	res, err := tb.Check(`China relaunches world's fastest train`,
+		`The top speed of the Fuxing or "rejuvenation" bullet trains was capped at 300km/h (186mph) in 2011 following two crashes that killed 40 people.
+From next week, some of the trains will once again be allowed to run at a higher speed of about 350 km/h.`,
+		u,
+	)
+	is.NoErr(err)
+
+	// Title
+	is.Equal(res.Title.Decision, "impartial")
+	is.True(res.Title.Score > 0.6)
+	is.Equal(len(res.Title.Entities), 1)
+	is.Equal(res.Title.Entities[0].Start, 0)
+	is.Equal(res.Title.Entities[0].End, 4)
+	is.Equal(res.Title.Entities[0].Text, "China")
+	is.Equal(res.Title.Entities[0].Type, "place")
+
+	// Domain
+	is.Equal(res.Domain.Category, "trusted")
+	is.Equal(res.Domain.Domain, "bbc.co.uk")
+
+	// Content
+	is.Equal(res.Content.Decision, "bias")
+	is.True(res.Content.Score < 0.4)
+	is.Equal(len(res.Content.Keywords), 10)
+	is.Equal(res.Content.Keywords[0].Keyword, "high speed")
+	is.Equal(res.Content.Keywords[1].Keyword, "bullet train")
+	is.Equal(len(res.Content.Entities), 8)
+	is.Equal(res.Content.Entities[0].Start, 21)
+	is.Equal(res.Content.Entities[0].End, 26)
+	is.Equal(res.Content.Entities[0].Text, "Fuxing")
+	is.Equal(res.Content.Entities[0].Type, "place")
+
+}
