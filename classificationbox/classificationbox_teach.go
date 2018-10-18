fe5b51e112f213e231b1bd4132ab125e0fcb382e
David Hernandez <dahernan@gmail.com>
2018-10-18 17:47:22 +0100

    cbox teach multi

--- classificationbox/classificationbox_teach.go
+++ classificationbox/classificationbox_teach.go
@@ -44,3 +44,35 @@ func (c *Client) Teach(ctx context.Context, modelID string, example Example) err
 	}
 	return nil
 }
+
+type examplesRequest struct {
+	Examples []Example `json:"examples"`
+}
+
+// TeachMulti gives an multiple Example to a model for it to learn from.
+func (c *Client) TeachMulti(ctx context.Context, modelID string, examples []Example) error {
+	u, err := url.Parse(c.addr + "/" + path.Join("classificationbox", "models", modelID, "teach-multi"))
+	if err != nil {
+		return err
+	}
+	if !u.IsAbs() {
+		return errors.New("box address must be absolute")
+	}
+	exreq := examplesRequest{Examples: examples}
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(exreq); err != nil {
+		return errors.Wrap(err, "encoding request body")
+	}
+	req, err := http.NewRequest(http.MethodPost, u.String(), &buf)
+	if err != nil {
+		return err
+	}
+	req = req.WithContext(ctx)
+	req.Header.Set("Accept", "application/json; charset=utf-8")
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	_, err = c.client.Do(req, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
